ddc2b820630697aa0033a5ec413be6804666ff4a
monica a <msagetmail@gmail.com>
2026-08-02 23:59:12 -0700

    Resolved conflict

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,11 +4,11 @@ import { Mail } from 'lucide-react'
 export default function Footer() {
   return (
     <footer className="flex items-center justify-between px-6 py-6 border-t text-sm text-gray-500">
-      <span>© {new Date().getFullYear()} Monica Ahuja/span>
+      <span>© {new Date().getFullYear()} Monica Ahuja</span>
       <div className="flex gap-4">
-        <a href="https://www.github.com/buddy2706/mainportfolio" target="_blank"><FaGithub size={18} /></a>
-        <a href="https://www.linkedin.com/in/monicaahuja" target="_blank"><FaLinkedin  size={18} /></a>
-        <a href="mailto:monica.s.ahuja@gmail.com"><Mail size={18} /></a>
+        <a href="https://github.com/PLACEHOLDER" target="_blank"><FaGithub size={18} /></a>
+        <a href="https://linkedin.com/in/PLACEHOLDER" target="_blank"><FaLinkedin size={18} /></a>
+        <a href="mailto:monica@saahasforcause.org"><Mail size={18} /></a>
       </div>
     </footer>
   )
