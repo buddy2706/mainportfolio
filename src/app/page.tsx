8e4e1392b7808eee73d49059049543fe8f815a96
MonicaA <127246153+buddy2706@users.noreply.github.com>
2026-08-02 22:46:29 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center px-6">
-      <h1 className="text-4xl font-bold">Monica A</h1>
+      <h1 className="text-8xl font-bold">Monica Ahuja</h1>
       <p className="mt-4 text-lg text-gray-600">
-        Senior Technical AI PM — building AI/ML products across cloud, nonprofit, and finance.
+        Senior Technical AI PM — building AI/ML products across B2C, healthcare, and finance.
       </p>
     </main>
   )
